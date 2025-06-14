69a335efbaaf00990bc353f423d09c5970db1354
Ashit-10 <95882932+Ashit-10@users.noreply.github.com>
2025-06-14 15:34:48 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        if f:
+            f.save(os.path.join(UPLOAD_FOLDER, f.filename))
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', files=files)
+
+@app.route('/files/<filename>')
+def serve_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+@app.route('/manifest.json')
+def manifest():
+    return app.send_static_file('manifest.json')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'icon.png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
